09c7689a010b271755a202c012cb7bc3781f0ea9
Vinicius Macedo <viniciusp.macedo@gmail.com>
2025-08-15 15:03:11 -0300

    first commit

---
+++ src/main/java/dev/viniciusmacedo/controlador_tarefas/ControladorTarefasApplication.java
@@ -0,0 +1,13 @@
+package dev.viniciusmacedo.controlador_tarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControladorTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControladorTarefasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/viniciusmacedo/controlador_tarefas/ControladorTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.viniciusmacedo.controlador_tarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControladorTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
