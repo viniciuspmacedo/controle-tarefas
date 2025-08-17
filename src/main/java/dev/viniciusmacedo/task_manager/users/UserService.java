79b9478b8441ffe2f776871f303d9a1be074e538
Vinicius Macedo <viniciusp.macedo@gmail.com>
2025-08-17 18:49:09 -0300

    feat: adiciona o arquivo UserService e implementa o metodo findAll

---
+++ src/main/java/dev/viniciusmacedo/task_manager/users/UserService.java
@@ -0,0 +1,19 @@
+package dev.viniciusmacedo.task_manager.users;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<UserModel> findAll(){
+        return userRepository.findAll();
+    }
+}
