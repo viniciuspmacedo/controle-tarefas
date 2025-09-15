4bd410665389af06021e1160101cdb0d69c9cd99
Vinicius Macedo <viniciusp.macedo@gmail.com>
2025-09-15 20:38:40 -0300

    doc: adiciona O Swagger na aplicação para documentação dos Controllers

--- src/main/java/dev/viniciusmacedo/task_manager/tasks/TaskController.java
+++ src/main/java/dev/viniciusmacedo/task_manager/tasks/TaskController.java
@@ -1,5 +1,9 @@
 package dev.viniciusmacedo.task_manager.tasks;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,7 +21,14 @@ public class TaskController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> getTaskById(@PathVariable Long id) {
+    @Operation(summary = "Busca tarefa por Id.", description = "Rota retorna uma tarefa pelo seu Id.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Tarefa encontrada."),
+            @ApiResponse(responseCode = "404", description = "Tarefa não encontrada.")
+    })
+    public ResponseEntity<?> getTaskById(
+            @Parameter(required = true, description = "Usuário envia o Id da tarefa no caminho da requisição.")
+            @PathVariable Long id) {
         if (taskService.findById(id) != null) {
             return ResponseEntity.ok(taskService.findById(id));
         } else {
@@ -26,6 +37,12 @@ public class TaskController {
     }
 
     @GetMapping()
+    @Operation(summary = "Retorna todas as tarefas", description = "Rota retorna uma lista contendo todas as tarefas cadastradas.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Tarefas listadas com sucesso."),
+            @ApiResponse(responseCode = "400", description = "Falha ao listar as tarefas")
+    }
+    )
     public ResponseEntity<?> findAll() {
         if (taskService.findAll().isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Tarefa não encontrada");
@@ -36,7 +53,16 @@ public class TaskController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<?> editTask(@PathVariable Long id, @RequestBody TaskDTO taskDTO) {
+    @Operation(summary = "Edita uma tarefa.", description = "Esta altera uma tarefa existente e salva as alterações no banco de dados")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Tarefa alterada com sucesso."),
+            @ApiResponse(responseCode = "400", description = "Erro na alteração da tarefa.")
+    })
+    public ResponseEntity<?> editTask(
+            @Parameter(required = true, description = "Usuário envia o Id da tarefa a ser alterada no caminho da requisição.")
+            @PathVariable Long id,
+            @Parameter(required = true, description = "Usuário envia os dados da tarefa no corpo da requisição.")
+            @RequestBody TaskDTO taskDTO) {
         if (taskService.findById(id) != null) {
             return ResponseEntity.ok(taskService.editById(id, taskDTO));
         } else {
@@ -45,12 +71,27 @@ public class TaskController {
     }
 
     @PostMapping
-    public ResponseEntity<TaskDTO> createTask(@RequestBody TaskDTO task) {
+    @Operation(summary = "Cria uma tarefa.", description = "Rota cria uma nova tarefa e a salva no banco de dados")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Tarefa criada com sucesso."),
+            @ApiResponse(responseCode = "400", description = "Erro ao criar a tarefa.")
+    })
+    public ResponseEntity<TaskDTO> createTask(
+            @Parameter(required = true, description = "Usuário envia os dados da nova tarefa no corpo da requisição.")
+            @RequestBody TaskDTO task) {
         return ResponseEntity.status(HttpStatus.CREATED).body(taskService.createTask(task));
     }
 
+
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> deleteTask(@PathVariable Long id) {
+    @Operation(summary = "Deleta uma tarefa", description = "Rota deleta uma tarefa com base em seu Id.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Tarefa deletada com sucesso."),
+            @ApiResponse(responseCode = "404", description = "Tarefa não encontrada.")
+    })
+    public ResponseEntity<?> deleteTask(
+            @Parameter(required = true, description = "Usuário envia o Id da tarefa a ser deletada no caminho da requisição")
+            @PathVariable Long id) {
         if (taskService.findById(id) != null) {
             taskService.deleteById(id);
             return ResponseEntity.noContent().build();
--- src/main/java/dev/viniciusmacedo/task_manager/users/UserController.java
+++ src/main/java/dev/viniciusmacedo/task_manager/users/UserController.java
@@ -1,10 +1,12 @@
 package dev.viniciusmacedo.task_manager.users;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/users")
@@ -17,7 +19,14 @@ public class UserController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> findUserById(@PathVariable Long id) {
+    @Operation(summary = "Busca usuário por Id.", description = "Rota retorna um usuário pelo seu Id.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Usuário encontrada."),
+            @ApiResponse(responseCode = "404", description = "Usuário não encontrada.")
+    })
+    public ResponseEntity<?> findUserById(
+            @Parameter(required = true, description = "Usuário envia o Id do usuário no caminho da requisição.")
+            @PathVariable Long id) {
         if (userService.findById(id) != null) {
             return ResponseEntity.ok(userService.findById(id));
         } else {
@@ -27,6 +36,12 @@ public class UserController {
     }
 
     @GetMapping
+    @Operation(summary = "Retorna todos os usuários", description = "Rota retorna uma lista contendo todos os usuários cadastrados.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Usuários listados com sucesso."),
+            @ApiResponse(responseCode = "400", description = "Falha ao listar os usuários")
+    }
+    )
     public ResponseEntity<?> findAll() {
         if (userService.findAll() != null) {
             return ResponseEntity.ok(userService.findAll());
@@ -36,7 +51,16 @@ public class UserController {
     }
 
     @PutMapping("{id}")
-    public ResponseEntity<?> editById(@PathVariable Long id, @RequestBody UserDTO userDTO) {
+    @Operation(summary = "Edita um usuário.", description = "Esta altera um usuário existente e salva as alterações no banco de dados")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Usuário alterado com sucesso."),
+            @ApiResponse(responseCode = "400", description = "Erro na alteração do usuário.")
+    })
+    public ResponseEntity<?> editById(
+            @Parameter(required = true, description = "Usuário envia o Id do usuário a ser alterado no caminho da requisição.")
+            @PathVariable Long id,
+            @Parameter(required = true, description = "Usuário envia os dados do usuário no corpo da requisição.")
+            @RequestBody UserDTO userDTO) {
         if (userService.findById(id) != null) {
             return ResponseEntity.ok(userService.editById(id, userDTO));
         } else {
@@ -45,12 +69,26 @@ public class UserController {
     }
 
     @PostMapping()
-    public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO user) {
+    @Operation(summary = "Cria um usuário.", description = "Rota cria um novo usuaário e salva no banco de dados")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Usuário criado com sucesso."),
+            @ApiResponse(responseCode = "400", description = "Erro ao criar o usuário.")
+    })
+    public ResponseEntity<UserDTO> createUser(
+            @Parameter(required = true, description = "Usuário envia os dados do novo usuário no corpo da requisição.")
+            @RequestBody UserDTO user) {
         return ResponseEntity.status(HttpStatus.CREATED).body(userService.createUser(user));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> deleteUser(@PathVariable Long id) {
+    @Operation(summary = "Deleta um usuário", description = "Rota deleta um usuário com base em seu Id.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Usuário deletado com sucesso."),
+            @ApiResponse(responseCode = "404", description = "Usuário não encontrada.")
+    })
+    public ResponseEntity<?> deleteUser(
+            @Parameter(required = true, description = "Usuário envia o Id do usuário a ser deletado no caminho da requisição")
+            @PathVariable Long id) {
         if (userService.findById(id) != null) {
             userService.deleteById(id);
             return ResponseEntity.noContent().build();
